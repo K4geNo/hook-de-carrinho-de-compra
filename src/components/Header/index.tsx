1a6197adc286f0e6bbc3dc8daf675f3803bc547e
K4geNo <micaraujo71@gmail.com>
2023-03-29 11:25:36 -0300

    challenge completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { MdShoppingBasket } from 'react-icons/md';
+import { Cart, Container } from './styles'
 
-import logo from '../../assets/images/logo.svg';
-import { Container, Cart } from './styles';
-import { useCart } from '../../hooks/useCart';
+import { Link } from 'react-router-dom'
+import { MdShoppingBasket } from 'react-icons/md'
+import logo from '../../assets/images/logo.svg'
+import { useCart } from '../../hooks/useCart'
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+    const { cart } = useCart()
 
-  return (
-    <Container>
-      <Link to="/">
-        <img src={logo} alt="Rocketshoes" />
-      </Link>
+    const cartSize = cart.length
 
-      <Cart to="/cart">
-        <div>
-          <strong>Meu carrinho</strong>
-          <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
-          </span>
-        </div>
-        <MdShoppingBasket size={36} color="#FFF" />
-      </Cart>
-    </Container>
-  );
-};
+    return (
+        <Container>
+            <Link to="/">
+                <img src={logo} alt="Rocketshoes" />
+            </Link>
 
-export default Header;
+            <Cart to="/cart">
+                <div>
+                    <strong>Meu carrinho</strong>
+                    <span data-testid="cart-size">
+                        {cartSize === 1
+                            ? `${cartSize} item`
+                            : `${cartSize} itens`}
+                    </span>
+                </div>
+                <MdShoppingBasket size={36} color="#FFF" />
+            </Cart>
+        </Container>
+    )
+}
+
+export default Header
